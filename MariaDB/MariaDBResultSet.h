363e2aeab699c307827df9e0a4d2ef4f24d1676c
Kieren Eaton <499977+circulon@users.noreply.github.com>
2025-06-24 18:18:03 +0800

    Added objectForColumn Method

--- MariaDB/MariaDBResultSet.h
+++ MariaDB/MariaDBResultSet.h
@@ -14,6 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (BOOL) next: (NSError*__autoreleasing*) error NS_SWIFT_NOTHROW;
 - (id) objectForColumnIndex: (NSUInteger) columnIndex;
+- (id) objectForColumn: (NSString*) columnName;
 
 @property(nonatomic,retain,readonly) NSArray<NSString*>* columnNames;
 
